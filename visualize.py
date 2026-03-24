638dc10175e73c38349a15528325aad973027dc1
Raneem Khaled <raneemkh2006@gmail.com>
2026-03-25 00:47:57 +0200

    Update visualize.py

---
+++ visualize.py
@@ -0,0 +1,18 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+num_cols = df.select_dtypes('number').columns
+fig, ax = plt.subplots(1, 3, figsize=(18, 5))
+
+sns.boxplot(y=df[num_cols[0]], ax=ax[0])
+ax[0].set_title('Boxplot')
+
+sns.histplot(df[num_cols[0]], bins=20, ax=ax[1])
+ax[1].set_title('Histogram')
+
+sns.heatmap(df[num_cols].corr(), annot=True, cmap='coolwarm', ax=ax[2])
+ax[2].set_title('Correlation Heatmap')
+
+plt.tight_layout()
+plt.savefig("summary_plot.png")
+plt.show()
