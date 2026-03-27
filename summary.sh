0ebc54fa0427b0a40c6a289f12b414bec14c10ff
Ranaa <ranamohsen@gmail.com>
2026-03-27 02:59:28 +0200

    Refactor summary.sh to standardize variable names and improve Docker image build process

--- summary.sh
+++ summary.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+IMAGE_NAME="cosmetics-analytics:latest"
+CONTAINER_NAME="analytics_container"
+HOST_RESULTS="./results"
+DATA_FILE="data.zip"
 
-CONTAINER_NAME=assignment-one-container
-HOST_RESULTS=./customer-analytics/results
 
-# Create results folder if not exists
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME .
+
 mkdir -p $HOST_RESULTS
+
+docker run --name $CONTAINER_NAME -v "$(pwd):/app/pipeline" $IMAGE_NAME python ingest.py $DATA_FILE
 
 echo "Copying output files from container..."
 
-docker cp $CONTAINER_NAME:/app/pipeline/data_raw.csv $HOST_RESULTS/
+# docker cp $CONTAINER_NAME:/app/pipeline/data_raw.csv $HOST_RESULTS/
 docker cp $CONTAINER_NAME:/app/pipeline/data_preprocessed.csv $HOST_RESULTS/
 docker cp $CONTAINER_NAME:/app/pipeline/insight1.txt $HOST_RESULTS/
 docker cp $CONTAINER_NAME:/app/pipeline/insight2.txt $HOST_RESULTS/
